d51b3c718da2fa79be59c05872f4c2978285a1de
Max <maksim_stepanov_06@mail.ru>
2019-12-04 19:09:00 +0300

    Add 6

---
+++ 1/src/Main/sadacha_2.java
@@ -0,0 +1,45 @@
+package Main;
+
+import java.util.Scanner;
+
+public class sadacha_2 {
+
+	 public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+		   System.out.print("Введи номер месяца: ");
+		 int mes=in.nextInt();
+		    String mesString;
+		    switch (mes) {
+		        case 1:  mesString = "Зима";
+		                 break;
+		        case 2:  mesString = "Зима";
+		                 break;
+		        case 3:  mesString = "Весна";
+		                 break;
+		        case 4:  mesString = "Весна";
+		                 break;
+		        case 5:  mesString = "Весна";
+		                 break;
+		        case 6:  mesString = "Лето";
+		                 break;
+		        case 7:  mesString = "Лето";
+		                 break;
+		        case 8:  mesString = "Лето";
+                         break;
+                case 9:  mesString = "Осень";
+                         break;
+                case 10: mesString = "Осень";
+                         break;
+                case 11: mesString = "Осень";
+                         break;
+                case 12: mesString = "Зима";
+                         break;
+                default: mesString = "Введите номер месяца 1-12!!!";
+		                 break;
+		    }
+		    System.out.print("Это время года: " + mesString);
+		    in.close();
+}
+
+
+}
